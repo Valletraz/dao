272d990c1e290d148ad3fa0ba52768bb1308b742
Valletraz <valletraz@yandex.ru>
2023-06-12 19:19:30 +0300

    D7_5__

--- config.py
+++
@@ -1,4 +0,0 @@
-REDIS_DB = 'redis://default:q9kOnWh87vrkSdcyltHbGO3jDSorhEIU@redis-19964.c8.us-east-1-2.ec2.cloud.redislabs.com:19964'
-YANDEX_EMAIL="valletraz@yandex.ru"
-YANDEX_PW = "suztvgexyplurlfr"
-DJANGO_KEY = 'django-insecure-e8e**!pq&64(jl=j^^mw$(n^kipqex=x*y%z60pc@x=c21-kca'
